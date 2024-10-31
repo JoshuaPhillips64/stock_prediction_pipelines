28948b4dd8ccbba3638e43db7ff6e92e48cdbc3c
jphil <jphillips@usserviceanimals.org>
2024-10-31 16:59:06 -0500

    AIrlfow Updates

--- src/airflow/dags/common/config.py
+++ src/airflow/dags/common/config.py
@@ -5,15 +5,10 @@ LAMBDA_FUNCTION_NAME = 'ingest_stock_data'
 POSTGRES_CONN_ID = 'postgres_default'
 
 # Top 50 stock tickers
-TOP_50_TICKERS_NEW = [
+TOP_50_TICKERS = [
     'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'META', 'NVDA', 'JPM', 'V', 'UNH',
     'HD', 'PG', 'BAC', 'MA', 'DIS', 'VZ', 'CMCSA', 'INTC', 'PFE', 'CSCO',
     'KO', 'ADBE', 'NFLX', 'PEP', 'CRM', 'ABT', 'JNJ', 'ORCL', 'ABBV', 'MRK',
     'MCD', 'COST', 'WMT', 'TMO', 'ACN', 'NKE', 'DHR', 'QCOM', 'MDT', 'LLY',
     'IBM', 'AMGN', 'TXN', 'NEE', 'AVGO', 'PM', 'UNP', 'HON', 'LIN', 'LOW', 'XOM', 'GIS', 'CL', 'MMM', 'SPY'
-]
-
-# Top 50 stock tickers
-TOP_50_TICKERS = [
-     'MMM'
 ]
